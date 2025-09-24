dbc92a3fcdeb2020bc3379adf9c2171cfe0b7771
Kuba Klin <poczta@kubaklin.pl>
2025-09-24 16:23:18 +0200

    feat: app init

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: {
+        buildMode: true,
+      },
+    })
+  ],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./testsSetup.tsx'],
+  }
+})
