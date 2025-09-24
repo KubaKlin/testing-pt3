702fd07bb55786eaa4461bc80837062048929c20
Kuba Klin <poczta@kubaklin.pl>
2025-09-24 18:31:01 +0200

    feat: config & root styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import checker from 'vite-plugin-checker';
 
@@ -10,10 +10,10 @@ export default defineConfig({
       typescript: {
         buildMode: true,
       },
-    })
+    }),
   ],
   test: {
     environment: 'jsdom',
     setupFiles: ['./testsSetup.tsx'],
-  }
-})
+  },
+});
